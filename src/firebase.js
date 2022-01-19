ff9e29d362587b002f9da76e87e0e590d467a682
Kritsada Tariya <mr.kritsada.t@gmail.com>
2022-01-19 18:53:52 +0700

    :beers: implement firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCXdas3BtmOzpgc9l8ucSQrKRQW_K92ikA",
     authDomain: "netflix-clone-5393f.firebaseapp.com",
@@ -7,3 +11,10 @@ const firebaseConfig = {
     appId: "1:231315787417:web:2ecb7668e3f1e1dfc4f208",
     measurementId: "G-1KPMXVZ9F8",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
